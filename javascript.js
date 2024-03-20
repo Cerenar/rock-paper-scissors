fd15661919b0c14dba346cda8dd9f1e0bce316bd
Cerenar <camerondonner63@gmail.com>
2024-03-20 10:55:30 -0500

    Add game end messages for win and loss

--- javascript.js
+++ javascript.js
@@ -15,33 +15,46 @@ function playRound(playerChoice) {
     let computerChoice = getComputerChoice();
     let resultMessage = document.createElement("p");
 
+    if (resultsField.children.length > 0) {
+        resultsField.removeChild(resultsField.lastChild);
+    }
+
     if (playerChoice === computerChoice) {
-        if (resultsField.children.length > 0) {
-            resultsField.removeChild(resultsField.lastChild);
-        }
         resultMessage.textContent = "Tie, play again!";
-        resultsField.appendChild(resultMessage);
     }
     else if ((playerChoice === "rock" && computerChoice === "scissors") || 
     (playerChoice === "scissors" && computerChoice === "paper") || 
     (playerChoice === "paper" && computerChoice === "rock")) {
-        if (resultsField.children.length > 0) {
-            resultsField.removeChild(resultsField.lastChild);
-        }
+        
         resultMessage.textContent = "Congrats, you win!";
-        resultsField.appendChild(resultMessage);
+        playerScore++;
     }
     else {
-        if (resultsField.children.length > 0) {
-            resultsField.removeChild(resultsField.lastChild);
-        }
         resultMessage.textContent = "Ah, too bad. Better luck next time!";
-        resultsField.appendChild(resultMessage);
+        computerScore++;
+    }
+
+    resultsField.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+    resultsField.appendChild(resultMessage);
+
+    if (playerScore > 4) {
+        let victoryMessage = document.createElement("h1");
+        victoryMessage.textContent = "You beat the computer! You're a rock, paper, scissors master!"
+        resultsField.appendChild(victoryMessage);
+    }
+    if (computerScore > 4) {
+        let lossMessage = document.createElement("h1");
+        lossMessage.textContent = "You lost to the computer. Come back after training some more..."
+        resultsField.appendChild(lossMessage);
     }
 }
 
 const resultsField = document.querySelector("div");
 const buttons = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
+
+resultsField.textContent = "Player: 0 Computer: 0";
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
